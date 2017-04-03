02d65a8444395306f2816a189d7e18e5a2a0f5ec
alisdair73 <alisdair73@gmail.com>
2017-04-03 18:15:38 +1000

    Initial Commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("warranty_prior_work_approval_inquiry.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("warranty_prior_work_approval_inquiry.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
